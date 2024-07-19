d1fbb35c67e6d42aa3a8d6af94da09b279793698
Lilibeth <lilibethocando@gmail.com>
2024-07-19 16:45:18 -0500

    Added more Templates example

--- app/routes.py
+++ app/routes.py
@@ -8,4 +8,15 @@ def index():
         "username" : "Lilibeth"
     }
 
-    return render_template("index.html", title="Home", user=user)
+    posts = [
+        {
+            'author': {'username': 'Michelle'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Ethan'},
+            'body': 'The Avengers movie was so coll!'
+        }
+    ]
+
+    return render_template("index.html", title="Home", user=user, posts=posts)
