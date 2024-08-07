28906e03d08b31633c7da690db0c7d88d2d6d17f
Lilibeth <lilibethocando@gmail.com>
2024-08-06 22:19:01 -0500

    Update for DB

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+
+class User(db.Model):
+    pass
--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
 
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '65484dxgdrgfcbv325698'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+
+print(os.path.join(basedir, 'app.db'))
