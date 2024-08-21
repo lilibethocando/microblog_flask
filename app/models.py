ff853b537efda56a8f5a8159a8697de968a35168
Lilibeth <lilibethocando@gmail.com>
2024-08-21 17:26:44 -0500

    New migration

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ class User(db.Model):
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
